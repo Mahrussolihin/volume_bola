ad3fdcef8cbb009a2bebe0f2409d6327d6731e02
Mahrussolihin <143859887+Mahrussolihin@users.noreply.github.com>
2023-09-06 22:29:19 +0700

    Add files via upload
    coding menghitung volume bola terbaru (sederhana)

--- volume_bola.py
+++ volume_bola.py
@@ -17,6 +17,32 @@ r = int(input("Masukkan jari jari bola = "))
 volume = 4/3*phi*(r*r*r)
 
 print()
+print("--------Hasilnya-------")
 print("Volume bola =", format(volume))
 print("Volume bola hasilnya adalah =", volume)
+#selesai
+print()
+print()
+
+#coding 2
+nama = "Mahrus Solihin"
+print(nama, "tipenya", type(nama))
+NIM = 230441100032
+print(NIM, "Tipenya", type(NIM))
+print()
+
+#coding rumus volume bola
+print("Rumus volume bola adalah v = 4/3*phi*(r**3)")
+print()
+
+phi = 3.14
+r = 28
+volume = 4/3*phi*(r**3)
+
+def volume(phi,r):
+    hasil = 4/3*phi*(r**3)
+    return hasil
+print()
+print("--------Hasilnya--------")
+print("volume bola =", volume(phi,r))
 #selesai
